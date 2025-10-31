8a014defe326d702ac302ebc12cd1144f69c6a1c
deepikavemuri <deepika.vemuri1@gmail.com>
2025-10-31 10:06:47 +0000

    Deploying to gh-pages from @ deepikavemuri/deepikavemuri.github.io@5dbcdfa9aaeafcb7adecb4602b002b88074482de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/al-folio/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/al-folio/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-paper-on-walking-the-web-of-concept-class-relationships-in-incrementally-trained-interpretable-models-accepted-at-aaai-2025",
+          title: 'Paper on “Walking the Web of Concept-Class Relationships in Incrementally Trained Interpretable Models”...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%65%65%70%69%6B%61.%76%65%6D%75%72%69%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/deepikavemuri", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +516,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/deepika-s-n-vemuri", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=vpTuijEAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
